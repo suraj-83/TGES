cdfc2316e9fb617cbc4f2e096d473352e494bfa1
suraj-83 <surajsharma24070@gmail.com>
2024-04-28 20:27:05 +0530

    add responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,21 +21,23 @@ const Home = () => {
   };
 
   return (
-    <div className='h-screen w-screen'>
-      <div className="min-h-screen w-full flex items-center justify-center relative">
-        <img src="https://wallpaperaccess.com/full/4823404.jpg" alt="" className='bgimg object-fill' />
-        <div className="max-w-4xl mx-auto p-8 absolute">
-          <h1 className="text-4xl md:text-6xl font-bold text-center text-grey-800 mb-8">
-            Explore the World
-          </h1>
-          <p className="text-lg md:text-xl text-grey-800 text-center mb-12">
-            Find your next adventure with us!
-          </p>
+    <div className='min-h-screen flex flex-col'>
+      <div className="flex-grow relative">
+        <img src="https://wallpaperaccess.com/full/4823404.jpg" alt="" className='bgimg object-fill w-full h-full' />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="max-w-4xl mx-auto p-8">
+            <h1 className="text-4xl md:text-6xl font-bold text-center text-gray-800 mb-8">
+              Explore the World
+            </h1>
+            <p className="text-lg md:text-xl text-gray-800 text-center mb-12">
+              Find your next adventure with us!
+            </p>
+          </div>
         </div>
       </div>
-      <div className="cards p-4 gap-4 h-[50%] w-full flex flex-row justify-between">
+      <div className="cards mt-5 gap-8 h-full w-full flex flex-wrap justify-center">
         <div
-          className="w-[40%] h-[40%] card"
+          className="w-full md:w-[40%] lg:w-[30%] h-[40%] card"
           onMouseEnter={() => handleHover(0)}
           onMouseLeave={() => handleMouseLeave(0)}
         >
@@ -43,11 +45,11 @@ const Home = () => {
             ref={(el) => (cardRefs.current[0] = el)}
             src="https://media.istockphoto.com/id/176981210/photo/zen-garden-stepping-stones-in-kyoto-japan.jpg?s=612x612&w=0&k=20&c=LW80F7_tHvkUNIIqnvsCy8LCuCp5CKIuADZ7wNc1EQI="
             alt=""
-            className='object-cover'
+            className='object-cover w-full h-full'
           />
         </div>
         <div
-          className="w-[40%] h-[40%] card"
+          className="w-full md:w-[40%] lg:w-[30%] h-[40%] card"
           onMouseEnter={() => handleHover(1)}
           onMouseLeave={() => handleMouseLeave(1)}
         >
@@ -55,11 +57,11 @@ const Home = () => {
             ref={(el) => (cardRefs.current[1] = el)}
             src="https://media.istockphoto.com/id/1342457449/photo/pamplona-navarra-spain-plaza-del-castillo-square-camino-de-santiago.jpg?s=612x612&w=0&k=20&c=pRQObkjWDMg1WKZItv95EoCTDVN5oD7s1X--rrIL9tM="
             alt=""
-            className='object-cover'
+            className='object-cover w-full h-full'
           />
         </div>
         <div
-          className="w-[40%] h-[40%] card"
+          className="w-full md:w-[40%] lg:w-[30%] h-[40%] card"
           onMouseEnter={() => handleHover(2)}
           onMouseLeave={() => handleMouseLeave(2)}
         >
@@ -67,11 +69,11 @@ const Home = () => {
             ref={(el) => (cardRefs.current[2] = el)}
             src="https://media.istockphoto.com/id/1460815824/photo/blue-hour-street.jpg?s=612x612&w=0&k=20&c=47DeK39Ba1RSS6XKUDY3WbWpgpQCvHfDPLlaGCtKXc4="
             alt=""
-            className='object-cover'
+            className='object-cover w-full h-full'
           />
         </div>
         <div
-          className="w-[40%] h-[40%] card"
+          className="w-full md:w-[40%] lg:w-[30%] h-[40%] card"
           onMouseEnter={() => handleHover(3)}
           onMouseLeave={() => handleMouseLeave(3)}
         >
@@ -79,11 +81,11 @@ const Home = () => {
             ref={(el) => (cardRefs.current[3] = el)}
             src="https://media.istockphoto.com/id/1546116710/photo/scenic-view-of-palma-de-mallorca-old-town-spain.jpg?s=612x612&w=0&k=20&c=zBKbSOMn4VZeL_QKZtzK7EpzeVwWgMZBUZbd0eIOw6U="
             alt=""
-            className='object-cover'
+            className='object-cover w-full h-full'
           />
         </div>
         <div
-          className="w-[40%] h-[40%] card"
+          className="w-full md:w-[40%] lg:w-[30%] h-[40%] card"
           onMouseEnter={() => handleHover(4)}
           onMouseLeave={() => handleMouseLeave(4)}
         >
@@ -91,14 +93,13 @@ const Home = () => {
             ref={(el) => (cardRefs.current[4] = el)}
             src="https://media.istockphoto.com/id/1370461598/photo/picturesque-autumn-scene-of-sunny-morning-on-almsee-lake-poppular-travell-destination.jpg?s=612x612&w=0&k=20&c=CL7XvGc_hK9_IG_c7NZrqLIAhBJgOmVR9Zc3YZwJ1Kk="
             alt=""
-            className='object-cover'
+            className='object-cover w-full h-full'
           />
         </div>
       </div>
-      <footer className="bg-gray-200 py-4">
-  <p className="text-center text-gray-800 text-sm">&copy; 2021 TGESTRAVEL. All rights reserved.</p>
-</footer>
-
+      <footer className="py-2 w-full md:py-4">
+        <p className="text-center text-gray-800 text-sm md:text-base">&copy; 2021 TGESTRAVEL. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
